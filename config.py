566b3cd201a48aec872b07867458c4bc39b9c91e
MMADPNL <mohamad23rezazade@gmail.com>
2026-08-30 13:54:18 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,23 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
+# آیدی عددی مالک
 OWNER_ID = 8552447077
 
+# عضویت اجباری
 FORCE_CHAT = "@LATAR_tek"
 
+# ارز مجازی
 CURRENCY = "DOGS"
 
-DICE_REWARD = 1000
-BOWLING_REWARD = 1000
-DART_REWARD = 1000
-BASKETBALL_REWARD = 500
-
+# تعداد راند مجاز
 MAX_ROUNDS = 4
 
+# ضریب‌ها
+DICE_MULTIPLIER = 2.0
+BOWLING_MULTIPLIER = 2.0
+DART_MULTIPLIER = 2.0
+BASKETBALL_MULTIPLIER = 1.5
+
+# دیتابیس
 DATABASE_PATH = "data/database.db"
